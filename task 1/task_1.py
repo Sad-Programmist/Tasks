0fe6c240b085f95c76f81147109e45180b827bc8
Yana <solodovnikovasyv@yandex.ru>
2022-03-04 13:51:40 +0300

    Edit task 1.

--- task 1/task_1.py
+++ task 1/task_1.py
@@ -1,7 +1,5 @@
 def progression(a):
-    min_quantity = len(a)
-    min_dif = 0
-    min_a1 = 0
+    min_quantity, min_dif, min_a1 = len(a), 0, 0
     for i in range(len(a) - 1):
         quantity = 0
         dif = a[i + 1] - a[i]
@@ -9,9 +7,7 @@ def progression(a):
             if a[j] != a[i] + dif * (j - i):
                 quantity += 1
         if quantity < min_quantity:
-            min_quantity = quantity
-            min_dif = dif
-            min_a1 = a[i] - dif * i
+            min_quantity, min_dif, min_a1 = quantity, dif, a[i] - dif * i
     return list(range(min_a1, min_a1 + min_dif * len(a), min_dif))
 
 
